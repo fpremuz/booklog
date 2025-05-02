525c12949c0009816682ad08f57c3f978910d965
Francisco Premuz <fpremuz@gmail.com>
2025-05-02 03:45:35 +0000

    redirect to index when logging out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate_by(params.permit(:email_address, :password))
       start_new_session_for user
-      redirect_to after_authentication_url, status: :see_other
+      redirect_to after_authentication_url
     else
       redirect_to new_session_path, alert: "Try another email address or password."
     end
@@ -16,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     terminate_session
-    redirect_to new_session_path
+    redirect_to books_path
   end
 end
