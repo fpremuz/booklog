995f424d8e90d1881149fc540168b665b61e0688
Francisco Premuz <fpremuz@gmail.com>
2025-05-02 05:09:51 +0000

    created users controller, improved views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get "users/new"
+  get "users/create"
+  get "users/show"
   resource :session
   resources :passwords, param: :token
   resources :books
+  resources :users, only: [:new, :create, :show]
 
   get "/login", to: "sessions#new", as: :login
   post "/login", to: "sessions#create"
