3c0c36ddfce5394b003e997dcfdb30e39598097c
Francisco Premuz <fpremuz@gmail.com>
2025-05-07 03:01:59 +0000

    fixed graphs

---
+++ app/javascript/chartkick_init.js
@@ -0,0 +1,4 @@
+import Chartkick from "chartkick"
+import Chart from "chart.js/auto"
+
+Chartkick.use(Chart)
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,9 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "chartkick" # @5.0.1
-pin "chart.js" # @4.4.9
-pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.4
+
+pin "chartkick", to: "chartkick.js"
+pin "chartkick_init", to: "chartkick_init.js"
+pin "chart.js/auto", to: "https://esm.sh/chart.js@4.4.1/auto"
