b755214512e4dc46db04f9efb607166f019f880d
Francisco Premuz <fpremuz@gmail.com>
2025-05-08 21:48:39 +0000

    fix for deploy

--- db/migrate/20250508040733_make_user_id_not_null_on_tags.rb
+++
@@ -1,5 +0,0 @@
-class MakeUserIdNotNullOnTags < ActiveRecord::Migration[8.0]
-  def change
-    change_column_null :tags, :user_id, false
-  end
-end
--- db/migrate/20250508213309_fix_null_user_id_in_tags.rb
+++ db/migrate/20250508213309_fix_null_user_id_in_tags.rb
@@ -1,7 +1,7 @@
 class FixNullUserIdInTags < ActiveRecord::Migration[8.0]
   def up
     default_user_id = User.first&.id
-    raise "No users exist in the database to assign to existing tags!" unless default_user_id
+    raise "No users found! Create at least one user before running this migration." unless default_user_id
 
     Tag.where(user_id: nil).update_all(user_id: default_user_id)
 
